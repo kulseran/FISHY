a9a5b3b300eeb7d4dbed2f98df34c9d25ea4d79e
kulseran <kulseran@users.noreply.github.com>
2017-11-25 18:35:50 -0800

    Noncopyable

---
+++ CORE/UTIL/noncopyable.h
@@ -0,0 +1,31 @@
+/**
+ * noncopyable.h
+ *
+ * Prevents object from implementing copy operations.
+ *   class foo : public noncopyable {
+ *   };
+ *
+ */
+#ifndef FISHY_NON_COPYABLE_H
+#define FISHY_NON_COPYABLE_H
+
+namespace core {
+namespace util {
+
+/**
+ * Classes inheriting from this will result in compile errors
+ * if they are attempted to be copied.
+ */
+class noncopyable {
+  public:
+  noncopyable() {}
+
+  private:
+  noncopyable(const noncopyable &) {}
+  noncopyable &operator=(const noncopyable &) { return *this; }
+};
+
+} // namespace util
+} // namespace core
+
+#endif
