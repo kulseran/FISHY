bdd8a1997a5ed5d762a77359ffb214d56b48871b
kulseran <kulseran@users.noreply.github.com>
2017-12-02 10:57:54 -0800

    Add debug-break in test runner

--- TESTS/main.cpp
+++ TESTS/main.cpp
@@ -7,6 +7,8 @@
 
 #include "testcase.h"
 
+#include <CORE/BASE/asserts.h>
+
 int main(int argc, char **argv) {
   std::cout << "# Testing for build: " << BUILD_BRANCH_ID << "@"
             << BUILD_VERSION_HASH << " built on " << BUILD_TIMESTAMP
@@ -15,5 +17,6 @@ int main(int argc, char **argv) {
   const int errorCount = testing::runRegisteredTests();
   std::cout << "# Done. With " << errorCount << " errors." << std::endl;
 
+  ASSERT(errorCount == 0);
   return ((errorCount == 0) ? 0 : 1);
 }
