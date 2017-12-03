739dd0ebd8afadd69ce3f4cd8f5b0875142956cc
kulseran <kulseran@users.noreply.github.com>
2017-12-03 15:19:28 -0800

    Regex

---
+++ CORE/UTIL/regex.h
@@ -0,0 +1,90 @@
+/**
+ * Regex matcher
+ */
+#ifndef FISHY_REGEX_H
+#define FISHY_REGEX_H
+
+#include <CORE/types.h>
+
+#include <stack>
+#include <string>
+
+namespace core {
+namespace util {
+namespace parser {
+
+/**
+ * Regular Expression pattern matcher
+ */
+class RegExPattern {
+  public:
+  /**
+   * Error codes from build process.
+   */
+  struct eBuildError {
+    enum type {
+      NONE,
+      MISSING_ESCAPE,
+      BAD_ESCAPE,
+      MISSING_PEREN,
+      MISSING_BRACE,
+      PEREN_INSIDE_BRACE,
+      RANGE_OUTSIDE_BRACE,
+      MISSING_OPERAND
+    };
+  };
+
+  RegExPattern();
+  ~RegExPattern();
+  RegExPattern(const RegExPattern &other);
+  RegExPattern &operator=(const RegExPattern &other);
+
+  /**
+   * Constructs a regex in place. Will die on error.
+   *
+   * @param pattern the regex pattern to build
+   */
+  RegExPattern(const std::string &pattern);
+
+  /**
+   * Replaces the regex in this pattern.
+   *
+   * @param pattern the regex pattern to build
+   */
+  eBuildError::type build(const std::string &pattern);
+
+  /**
+   * Scans a string for a match against this pattern.
+   * The pattern matching starts at {@code begin} and continues until the first
+   * of
+   * {@code end} or match rejection.
+   *
+   * @param begin start of string to match
+   * @param end end of string to match
+   * @return iterator at end of match
+   */
+  std::string::const_iterator scan(
+      const std::string::const_iterator &begin,
+      const std::string::const_iterator &end) const;
+
+  /**
+   * Matches a full string against this pattern.
+   *
+   * @param begin start of string to match
+   * @param end end of string to match
+   * @return true if string is fully matched
+   */
+  bool match(
+      const std::string::const_iterator &begin,
+      const std::string::const_iterator &end) const;
+
+  private:
+  class Nfa;
+  Nfa *m_machine;
+};
+
+} // namespace parser
+} // namespace util
+} // namespace core
+
+#endif
